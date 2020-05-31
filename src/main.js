de2067198c8ff33c129375dcbba6826f00e4ba98
ruri03333 <yurie4241@gmail.com>
2020-05-31 17:26:11 +0900

    firebase-second

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from "firebase"
 
 Vue.config.productionTip = false
+const config = {
+  apiKey: 'AIzaSyA8BP7ZBxX1u3p2mjSS1TfFkXeV59EOotk',
+  projectId: 'firesecond-50fe1',
+  authDomain: 'firesecond-50fe1.firebaseapp.com',
+  databaseURL: 'https://firesecond-50fe1.firebaseio.com',
+  storageBucket: 'firesecond-50fe1.appspot.com',
+}
+firebase.initializeApp(config);
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
 
+export default firebase;
 new Vue({
   router,
   store,
